087a637c9b82ab5652eb969de15b3e5def14fa2a
rfp5221 <rfp5221@psu.edu>
2020-11-18 16:05:17 -0500

    Finished TODO 5, 8, 9

--- src/Controller/DeleteProducts.java
+++ src/Controller/DeleteProducts.java
@@ -2,9 +2,12 @@ package Controller;
 
 import Model.Product;
 import Model.ShoppingList;
+import org.apache.log4j.Logger;
 
 public class DeleteProducts {
   // TODO 09 - DeleteProducts: Make a debug log entry for each item deleted (See GrocerEase.log sample in assignment)
+  // Rushi Patel (rfp5221@psu.edu; github:rfp5221)
+  private static Logger logger = Logger.getLogger(DeleteProducts.class.getName());
 
   public static void deleteProducts(ShoppingList shoppingList) {
     while (true) {
--- src/Controller/ImportExport.java
+++ src/Controller/ImportExport.java
@@ -17,3 +17,4 @@ public class ImportExport {
     return imported;
   }
 }
+
